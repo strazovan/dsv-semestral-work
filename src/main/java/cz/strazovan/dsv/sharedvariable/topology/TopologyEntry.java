1e915157181bc1242b7f34c7f5816e789b759016
Stražovan David <strazovan.d@gmail.com>
2019-12-08 19:06:45 +0100

    adds comparator to topology entry

--- src/main/java/cz/strazovan/dsv/sharedvariable/topology/TopologyEntry.java
+++ src/main/java/cz/strazovan/dsv/sharedvariable/topology/TopologyEntry.java
@@ -1,13 +1,15 @@
 package cz.strazovan.dsv.sharedvariable.topology;
 
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Comparator;
 import java.util.Objects;
 
-public class TopologyEntry {
+public class TopologyEntry implements Comparable<TopologyEntry> {
     private final InetAddress address;
     private final int port;
+    private final Comparator<TopologyEntry> comparator = Comparator.comparing(TopologyEntry::getAddress, new InetAddressComparator())
+            .thenComparingLong(TopologyEntry::getPort);
 
     public TopologyEntry(InetAddress address, int port) {
         this.address = address;
@@ -44,4 +46,29 @@ public class TopologyEntry {
     public int hashCode() {
         return Objects.hash(address, port);
     }
+
+    @Override
+    public int compareTo(TopologyEntry other) {
+        return this.comparator
+                .compare(this, other);
+    }
+
+    // https://stackoverflow.com/questions/13756235/how-to-sort-ip-address-in-ascending-order
+    public static class InetAddressComparator implements Comparator<InetAddress> {
+        @Override
+        public int compare(InetAddress a, InetAddress b) {
+            byte[] aOctets = a.getAddress(),
+                    bOctets = b.getAddress();
+            int len = Math.max(aOctets.length, bOctets.length);
+            for (int i = 0; i < len; i++) {
+                byte aOctet = (i >= len - aOctets.length) ?
+                        aOctets[i - (len - aOctets.length)] : 0;
+                byte bOctet = (i >= len - bOctets.length) ?
+                        bOctets[i - (len - bOctets.length)] : 0;
+                if (aOctet != bOctet) return (0xff & aOctet) - (0xff & bOctet);
+            }
+            return 0;
+        }
+    }
+
 }
